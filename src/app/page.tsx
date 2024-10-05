3072cc43e5202bd6de727c1fff10d9325697b360
Dale-Xu1 <dalexu1@outlook.com>
2024-10-05 13:38:06 -0400

    CSS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,26 +7,15 @@ export default function Home() {
 	let [isSystemThinking, setIsSystemThinking] = useState(false);
 	let [messages, setMessages] = useState([
 		{
-			user: true,
-			text: "Example question",
-		},
-		{
 			user: false,
-			text: "Example response",
-		},
-		{
-			user: true,
-			text: "Example question again",
-		},
-		{
-			user: false,
-			text: "Example response again",
+			text: "Hi! How can I help you today?",
 		},
 	]);
 
 	async function onSubmit(e: FormEvent) {
 		e.preventDefault();
 		setIsSystemThinking(true);
+        setInput("")
 		const userMsg = { user: true, text: input };
 		setMessages([...messages, userMsg]);
 		let systemMsg;
@@ -47,16 +36,16 @@ export default function Home() {
 		<div className="main">
 			<div className="header">
 				<span className={poppins.className}>BUSource</span>
-			</div>
+			</div>  
 			<div className="body">
-				<div className="body-main">
-					{messages.map((msg, i) => (
-						<div className="response" key={i}>
-							{msg.user ? <span className={poppins.className}>Me:</span> : ""}
-							<span className={poppins.className}>{msg.text}</span>
-						</div>
-					))}
-				</div>
+                <div className="body-main">
+                    {messages.map((msg, i) => (
+                        <div className="response" key={i}>
+                            {msg.user ? <span className={poppins.className}>Me:</span> : ""}
+                            <span className={poppins.className}>{msg.text}</span>
+                        </div>
+                    ))}
+                </div>
 			</div>
 			<div className="footer">
 				<div className="footer-body">
